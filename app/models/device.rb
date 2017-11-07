09b32d6b41359a0d663f18bf972f48182f1e2f5f
liupc-ubuntu <liu_xiaodao@163.com>
2017-11-07 21:58:52 +0800

    ...

--- app/models/device.rb
+++ app/models/device.rb
@@ -2,8 +2,8 @@ class Device < ApplicationRecord
 	belongs_to :user, optional: true
 
 	validates :asset_code,  :asset_name,  :service_sn, :decategory_id, :receive_date, :asset_details, :belong_to, :status, presence: true   #这几个变量不能为空
-  	validates :asset_code,  :service_sn,  :asset_details,   length: { in: 6..25 } #长度6-25
-                       																					
+  	validates :asset_code,  :asset_name,  :service_sn,  :asset_details,   length: { in: 6..25 } #长度6-25
+
 
 end
 
