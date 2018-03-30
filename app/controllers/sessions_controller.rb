38010b71fd2c941331136833a0550a83719a1543
liuchunliang <Chunliang.Liu@abcam.com>
2018-03-30 14:25:54 +0800

    改变登录页面

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
   skip_before_action :check_signed_in, only: [:new, :create]      #整个系统都需要登录操作所以在application中设置,检测是都登录,但是在登录和注册页面需要跳过验证
 
-  layout 'login'
+  layout 'home'
 
   def new
     # 注册/登录同页面, 同时使用User_Model中的验证返回信息
@@ -14,7 +14,6 @@ class SessionsController < ApplicationController
         @user = User.find_by email: session_param(:email)
         if @user && @user.compare(session_param(:password))
             sign_in @user     #用户名加入session
-            session_param(:remember_me) == "1" ? remember_me(@user) : forget_me(@user)
             redirect_back_or root_path
         elsif @user
           # 密码错误
@@ -26,7 +25,6 @@ class SessionsController < ApplicationController
     else
         @user = User.from_omniauth(request.env["omniauth.auth"])      #这是通过ldap认证后,返回邮箱,再用邮箱找到用户,在返回用户
         sign_in @user
-        session_param(:remember_me) == "1" ? remember_me(@user) : forget_me(@user)
         redirect_back_or root_path
     end
   end
