bf4b223eb635d100badd698756f36f1aed300587
liuchunliang <Chunliang.Liu@abcam.com>
2018-04-09 16:06:06 +0800

    删除无用文件

--- sh/mysql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-USER="root"
-PASSWORD="123456"
-DATABASE="itmanager_development"
-TABLE="devices"
-mysql -u$USER -p$PASSWORD << EOF
-use $DATABASE
-update $TABLE set borrow_timeleft=borrow_timeleft-1 where status=4 and borrow_timeleft>0
-EOF
