3ac2a2b773c6851db0b2c1f2c25cc0a8ed8bc6f2
liuchunliang <Chunliang.Liu@abcam.com>
2018-04-13 17:45:47 +0800

    缺少引号

--- config/deploy.rb
+++ config/deploy.rb
@@ -61,7 +61,7 @@ set :keep_releases, 10
 
 set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :linked_dirs, fetch(:linked_dirs) + %w{public/uploads public/images}
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml, 'config/email.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/email.yml')
 
 set :config_dirs, %W{config config/environments/#{fetch(:stage)} public/system public/uploads}
 set :config_files, %w{config/database.yml config/secrets.yml  config/email.yml}
