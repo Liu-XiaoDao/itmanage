0dd08f28dddcc48d2c5615ec666dff2704a94ee9
liuchunliang <Chunliang.Liu@abcam.com>
2018-04-18 09:37:46 +0800

    只统计未离职员工

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -64,10 +64,6 @@ class StatisticsController < ApplicationController
     #拿到要显示的部门
     @department = Department.find(params[:id])
     #本部门及下级部门所有员工
-    @departmentUsers = User.joins("INNER JOIN departments ON users.department_id = departments.id AND departments.pgcode like '#{@department.pgcode}%'")
-    #可能会有很多下级部门
-    @lowerdepartments = @department.lowers
-    #添加下级部门的时候的下拉框中显示所有员工
-    @users = User.all
+    @departmentUsers = User.joins("INNER JOIN departments ON users.department_id = departments.id where users.is_quit = 0 AND departments.pgcode like '#{@department.pgcode}%'")
   end
 end
