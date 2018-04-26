8c58ee1deddc75089ae6e5645e57b49df3e92171
liuchunliang <Chunliang.Liu@abcam.com>
2018-04-26 15:49:57 +0800

    统计增加一个员工借用列

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -15,7 +15,7 @@ class StatisticsController < ApplicationController
     user_model_configs = current_user.user_model_configs.where(model: "custom_statistics").first
     select_fields = user_model_configs.present? ? user_model_configs.fields : []
 
-    fields = ["部门名称", "人数", "电脑", "比例", "电脑(接设备)"]
+    fields = ["部门名称", "人数", "电脑", "比例", "电脑(员工借用)", "电脑(接设备)"]
     if select_fields.present?
       fields = fields + ["设备总数",] + select_fields.collect{|field| Decategory.find(field).name}
     end
@@ -46,26 +46,31 @@ class StatisticsController < ApplicationController
       worksheet.add_cell(row+1, 1, departmentUsers.count)
 
       departstaffuse = 0
-      departmentUsers.each{ |user| departstaffuse = departstaffuse + user.statistic_devices([1,2]).where(status: [2,3]).count }
+      departmentUsers.each{ |user| departstaffuse = departstaffuse + user.statistic_devices([1,2]).where(status: [2]).count }
       worksheet.add_cell(row+1, 2, departstaffuse)
 
       #比例
       worksheet.add_cell(row+1, 3, "#{(departstaffuse.to_f/departmentUsers.count.to_f*100).round(1)}%")
 
+      #部门员工借用的电脑
+      brodepartmentdevicecount = 0
+      departmentUsers.each{ |user| brodepartmentdevicecount = brodepartmentdevicecount + user.statistic_devices([1,2]).where(status: 3).count }
+      worksheet.add_cell(row+1, 4, brodepartmentdevicecount)
+
       #部门接设备的电脑
       condepartmentdevicecount = 0
       departmentUsers.each{ |user| condepartmentdevicecount = condepartmentdevicecount + user.statistic_devices([1,2]).where(status: 4).count }
-      worksheet.add_cell(row+1, 4, condepartmentdevicecount)
+      worksheet.add_cell(row+1, 5, condepartmentdevicecount)
 
       if select_fields.present?
         departmentdevicecount = 0
         departmentUsers.each{ |user| departmentdevicecount = departmentdevicecount + user.statistic_devices(select_fields).count }
-        worksheet.add_cell(row+1, 5, departmentdevicecount)
+        worksheet.add_cell(row+1, 6, departmentdevicecount)
 
         select_fields.each_with_index do |field, col|
           tempdecategorydevicecount = 0
           departmentUsers.each{ |user| tempdecategorydevicecount = tempdecategorydevicecount + user.statistic_devices(field).count }
-          worksheet.add_cell(row+1, col+6, tempdecategorydevicecount)
+          worksheet.add_cell(row+1, col+7, tempdecategorydevicecount)
         end
       end
 
