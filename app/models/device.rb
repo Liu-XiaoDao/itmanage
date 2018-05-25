d776e53997748a814703ee73477fa4c279431f3d
liuchunliang <Chunliang.Liu@abcam.com>
2018-05-25 14:39:09 +0800

    设备维保邮件按照设备分类排序发送

--- app/models/device.rb
+++ app/models/device.rb
@@ -7,21 +7,25 @@ class Device < ApplicationRecord
 
 
 	#设备曾经有过哪些使用者
-    has_many :devicerecords, dependent: :destroy
-    has_many :onceusers, through: :devicerecords
+  has_many :devicerecords, dependent: :destroy
+  has_many :onceusers, through: :devicerecords
 
 	#授权分配记录
 	has_many :authorization_user_devices
 	has_many :authorizations, through: :authorization_user_devices
 
 	validates :asset_name,  :decategory_id, :release_date,  presence: true  #这几个不能为空
-  	validates :asset_name,  length: { in: 2..25 } #设备名长度6-25
+  validates :asset_name,  length: { in: 2..25 } #设备名长度6-25
 
 	validates :asset_code,  uniqueness: { case_sensitive: false }#设备编码要唯一
 
-    def self.to_xlsx(records)
-      export_fields = ["id", "asset_code", "asset_name", "service_sn", "asset_details", "status", "user_id", "location"]
-      SpreadsheetService.new.generate(export_fields, records)
-    end
-end
 
+	def device_category_name
+		Decategory.find_by_id(self.decategory_id).try(:name)
+	end
+
+  def self.to_xlsx(records)
+    export_fields = ["id", "asset_code", "asset_name", "service_sn", "asset_details", "status", "user_id", "location"]
+    SpreadsheetService.new.generate(export_fields, records)
+  end
+end
